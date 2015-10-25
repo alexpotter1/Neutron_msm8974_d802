3c2b141fdcf5a51bf76bd668b5d1e710f60af5b6
Alex Potter <alexanderpotter2010@gmail.com>
2015-10-25 16:26:41 +0000

    build: Move to new zip packaging process
    First commit of new zip system, where the installer script can generate the boot.img on the fly using the ramdisk from the previous kernel zImage.
    This is considered experimental at this stage.

---
+++ zip/setup/createbootimg.sh
@@ -0,0 +1,7 @@
+#!/sbin/sh
+echo \#!/sbin/sh > /tmp/createnewboot.sh
+echo /tmp/mkbootimg --kernel /tmp/zImage-dtb --ramdisk /tmp/boot.img-ramdisk.gz --cmdline \"$(cat /tmp/boot.img-cmdline)\" --base 0x$(cat /tmp/boot.img-base) --pagesize $(cat /tmp/boot.img-pagesize) --ramdisk_offset 0x$(cat /tmp/boot.img-ramdiskoff) --tags_offset 0x$(cat /tmp/boot.img-tagsoff) --dt /tmp/dtb --output /tmp/newboot.img >> /tmp/createnewboot.sh
+echo "dd if=/tmp/bump >> /tmp/newboot.img" >> /tmp/createnewboot.sh
+chmod 777 /tmp/createnewboot.sh
+/tmp/createnewboot.sh
+return $?
